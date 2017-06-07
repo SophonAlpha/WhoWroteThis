be3be1746be45f84eb4ff805b26d4bf698bbe1d9
SophonAlpha <a@b.com>
2017-06-07 22:03:00 +0400

    fixed 'invalid python module name' warning

--- pull_LinkedIn_articles.py
+++ pull_LinkedIn_articles.py
@@ -7,9 +7,7 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
-from click._compat import raw_input
 import time
 import re
 import os
